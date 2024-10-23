d8b529fc0e402aa179e2d3b16987caa953843676
Sheikh Jabed <jabedbd2295@gmail.com>
2024-10-23 23:50:40 +0600

    Navbar Section Updated

--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -1,11 +1,20 @@
 const Navbar = () => {
     return (
-        <div>
-            <div className="navbar-start">
+        <div className="flex justify-between items-center">
+            <div className="navbar-left">
             <h2 className="tex-4xl font-bold">Chef<span className="text-red-500">'</span>s Table</h2>
             </div>
-            <div className="">
-
+            <div className="navbar-center">
+                <ul className="flex justify-center items-center space-x-4">
+                    <li>Home</li>
+                    <li>Recipes</li>
+                    <li>About</li>
+                    <li>Search</li>
+                </ul>
+            </div>
+            <div className="navbar-right">
+                
+                <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto rounded-full bg-gray-100" />
             </div>
         </div>
     );
